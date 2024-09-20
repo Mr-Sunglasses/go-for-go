c5c41b2a879ec566478e31aad7eb061dfcd4076f
Kanishk Pachauri <itskanishkp.py@gmail.com>
2024-09-20 10:12:25 +0530

    learned the basics of go

---
+++ src/main.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+
+	// string
+	// var nameOne string = "mario"
+	// var nameTwo = "luigi"
+	// var nameThree string
+
+	// fmt.Println(nameOne)
+	// fmt.Println(nameTwo)
+	// fmt.Println(nameThree)
+
+	// nameOne = "peach"
+	// nameThree = "bowser"
+
+	// fmt.Println(nameOne)
+	// fmt.Println(nameTwo)
+	// fmt.Println(nameThree)
+
+	// nameFour := "yoshi"
+	// fmt.Println(nameFour)
+	// nameFour = "Toshi"
+	// fmt.Println(nameFour)
+
+	// ints
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// bits and memeroy
+	var numOne int8 = 120
+	var numTwo int16 = 1200
+	var numThree int32 = 12000
+	var numFour int64 = 1200000
+	var numFive uint8 = 25
+
+	fmt.Println(numOne, numTwo, numThree, numFour, numFive)
+
+	// var scoreOne float32 = 25.98
+	// var scoreTwo float64 = 8888888888.7
+	// scoreThree := 15.23
+}
